c97155c1ce5240d0f2297392f7ec4e884881d039
Dylan <dylan.reeder5@gmail.com>
2022-12-06 18:28:40 -0500

    able to broadcast a chat message

--- screens/Chats.tsx
+++ screens/Chats.tsx
@@ -1,48 +1,50 @@
 import { NavigationState } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
-import { io } from 'socket.io-client';
+import { FlatList, SafeAreaView, Text, TextInput, View } from 'react-native';
+import io from 'socket.io-client';
 import Button from '../components/button';
 import Layout from '../layouts/layout';
 // import { socket } from '../utils/socket';
+const socket = io('http://localhost:3000');
 
 export default function Chats({ navigation, route }: any) {
   const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState({});
-  const { socket } = route.params;
-  console.log(socket);
+  const [messages, setMessages] = useState([]);
+  // const { socket } = route.params;
+
+  // const sendMessage = (msg: string) => {
+  //   socket.emit('chat-message', msg);
+  // };
 
   useEffect(() => {
-    const messageListener = (message) => {
-      setMessages((prevMessages) => {
-        const newMessages = { ...prevMessages };
-        newMessages[message.id] = message;
-        return newMessages;
-      });
-    };
+    socket.on('recieve-message', (data: any) => {
+      setMessages([...messages, data.message]);
+      // console.log(data.message);
+    });
 
-    socket.on('chat message', messageListener);
-
-    return () => {
-      socket.off('chat message', messageListener);
-    };
+    console.log(messages);
   }, [socket]);
 
-  console.log(messages);
-
   return (
-    <View className='flex-1 bg-primaryDark'>
+    <View className='flex-1 bg-primaryDark px-4'>
       <Text className='text-white'>test</Text>
       <TextInput
         value={message}
         onChangeText={(text) => setMessage(text)}
-        className='bg-secondaryWhite text-black'
+        className='bg-secondaryDark text-white p-4 rounded-md'
       />
+      {messages.length > 0 && (
+        <FlatList
+          className='p-4'
+          data={messages}
+          renderItem={(msg) => <Text className='text-white'>{msg.item}</Text>}
+        />
+      )}
       {socket && (
         <Button
           variant='primary'
           onPress={() => {
-            socket.emit('chat message', message);
+            socket.emit('chat-message', { message: message });
             setMessage('');
           }}
         >

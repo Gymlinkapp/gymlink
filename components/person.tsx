3eece145f25630f76d84b006e7ddec5f176043ae
Dylan <dylan.reeder5@gmail.com>
2022-12-28 08:36:57 -0500

    able to navigate through images

--- components/person.tsx
+++ components/person.tsx
@@ -9,6 +9,7 @@ import {
   Image as RNImage,
   Animated,
   Easing,
+  TouchableOpacity,
 } from 'react-native';
 import { COLORS } from '../utils/colors';
 import { NAVBAR_HEIGHT, SPACING } from '../utils/sizes';
@@ -19,6 +20,7 @@ import Button from './button';
 export default function Person({ user }: { user: Partial<User> }) {
   const { width, height } = Dimensions.get('window');
   const [sentFriendRequest, setSentFriendRequest] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const fade = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
@@ -57,115 +59,81 @@ export default function Person({ user }: { user: Partial<User> }) {
     outputRange: [1, 0],
   });
   return (
-    <Animated.View
-      className='overflow-hidden rounded-2xl relative'
-      style={{
-        width: '100%',
-        height: CARD_HEIGHT,
-        marginVertical: SPACING,
-        position: 'relative',
-      }}
-    >
+    <>
+      <View className={`absolute top-0 left-0 w-full h-full flex-row z-50`}>
+        {/* back photo side */}
+        <TouchableOpacity
+          className='flex-1'
+          onPress={() =>
+            setCurrentImageIndex((prev) => {
+              if (prev === 0) {
+                return user.images.length - 1;
+              }
+              return prev - 1;
+            })
+          }
+        ></TouchableOpacity>
+        {/* forward photo side */}
+        <TouchableOpacity
+          className='flex-1 h-full'
+          onPress={() =>
+            setCurrentImageIndex((prev) => {
+              if (prev === user.images.length - 1) {
+                return 0;
+              }
+              return prev + 1;
+            })
+          }
+        ></TouchableOpacity>
+      </View>
       <Animated.View
+        className='overflow-hidden rounded-2xl relative'
         style={{
-          borderWidth: 15,
-          // transform: [{ scale: fade }],
-          // use the scale transform but make it bigger than 1
-          // so that the border is outside the image
-          transform: [
-            {
-              scale: fade.interpolate({
-                inputRange: [0, 1],
-                outputRange: [1.1, 1],
-              }),
-            },
-          ],
-          borderColor: gold,
-          zIndex: 10,
-          opacity: fade,
-          position: 'absolute',
-          top: 0,
-          left: 0,
           width: '100%',
-          height: '100%',
+          height: CARD_HEIGHT,
+          marginVertical: SPACING,
+          position: 'relative',
         }}
       >
-        <LinearGradient
-          colors={[gold, transparentGold]}
-          className='w-full h-full absolute top-0 left-0'
-          start={[0, 0]}
-          end={[0, 1]}
-        />
-      </Animated.View>
-      <RNImage source={{ uri: user.images[0] }} className='w-full h-full' />
-      <View className={`absolute bottom-0 left-0 w-full z-20 p-2`}>
         <Animated.View
           style={{
+            borderWidth: 15,
+            // transform: [{ scale: fade }],
+            // use the scale transform but make it bigger than 1
+            // so that the border is outside the image
             transform: [
               {
-                translateY: fade.interpolate({
+                scale: fade.interpolate({
                   inputRange: [0, 1],
-                  outputRange: [0, -20],
+                  outputRange: [1.1, 1],
                 }),
               },
             ],
+            borderColor: gold,
+            zIndex: 10,
+            opacity: fade,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
           }}
-          className='rounded-2xl overflow-hidden w-full'
         >
-          <BlurView className='w-full p-4' intensity={20}>
-            <Text className='text-white pb-2 text-xl font-bold'>
-              {user.firstName} {user.lastName}
-            </Text>
-            <Text className='text-secondaryWhite text-sm leading-4'>
-              {/* show the user's bio but if it is more than 30 characters truncate it */}
-              {user.bio.length > 100
-                ? user.bio.substring(0, 100) + '...'
-                : user.bio}
-            </Text>
-          </BlurView>
+          <LinearGradient
+            colors={[gold, transparentGold]}
+            className='w-full h-full absolute top-0 left-0'
+            start={[0, 0]}
+            end={[0, 1]}
+          />
         </Animated.View>
-        <View className='flex-row'>
+        <RNImage
+          source={{ uri: user.images[currentImageIndex] }}
+          className='w-full h-full'
+        />
+        <View className={`absolute bottom-0 left-0 w-full z-20 p-2`}>
           <Animated.View
             style={{
               transform: [
-                {
-                  scale: fade.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0.9, 1],
-                  }),
-                },
-                {
-                  translateY: fade.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0, 20],
-                  }),
-                },
-              ],
-              width: sendFriendRequest ? '0%' : '100%',
-              flex: sentFriendRequest ? 0 : 1,
-              opacity: fade.interpolate({
-                inputRange: [0, 1],
-                outputRange: [1, 0],
-              }),
-            }}
-          >
-            <Button
-              variant='secondary'
-              icon={<X weight='fill' color={COLORS.mainWhite} size={24} />}
-            >
-              Go Next
-            </Button>
-          </Animated.View>
-          <Animated.View
-            style={{
-              transform: [
-                {
-                  scale: fade.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0.9, 1],
-                  }),
-                },
-
                 {
                   translateY: fade.interpolate({
                     inputRange: [0, 1],
@@ -173,19 +141,99 @@ export default function Person({ user }: { user: Partial<User> }) {
                   }),
                 },
               ],
-              flex: 1,
             }}
+            className='rounded-2xl overflow-hidden w-full'
           >
-            <Button
-              onPress={() => setSentFriendRequest(!sentFriendRequest)}
-              variant='primary'
-              icon={<Barbell weight='fill' />}
-            >
-              Go Gym
-            </Button>
+            <BlurView className='w-full p-4' intensity={20}>
+              <Text className='text-white pb-2 text-xl font-bold'>
+                {user.firstName} {user.lastName}
+              </Text>
+              <Text className='text-secondaryWhite text-sm leading-4'>
+                {/* show the user's bio but if it is more than 30 characters truncate it */}
+                {user.bio.length > 100
+                  ? user.bio.substring(0, 100) + '...'
+                  : user.bio}
+              </Text>
+            </BlurView>
           </Animated.View>
+          <View className='flex-row'>
+            <Animated.View
+              style={{
+                transform: [
+                  {
+                    scale: fade.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0.9, 1],
+                    }),
+                  },
+                  {
+                    translateY: fade.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, 20],
+                    }),
+                  },
+                ],
+                width: sendFriendRequest ? '0%' : '100%',
+                flex: sentFriendRequest ? 0 : 1,
+                opacity: fade.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [1, 0],
+                }),
+              }}
+            >
+              <Button
+                variant='secondary'
+                icon={<X weight='fill' color={COLORS.mainWhite} size={24} />}
+              >
+                Go Next
+              </Button>
+            </Animated.View>
+            <Animated.View
+              style={{
+                transform: [
+                  {
+                    scale: fade.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0.9, 1],
+                    }),
+                  },
+
+                  {
+                    translateY: fade.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, -20],
+                    }),
+                  },
+                ],
+                flex: 1,
+              }}
+            >
+              <Button
+                onPress={() => setSentFriendRequest(!sentFriendRequest)}
+                variant='primary'
+                icon={<Barbell weight='fill' />}
+              >
+                Go Gym
+              </Button>
+            </Animated.View>
+          </View>
         </View>
-      </View>
-    </Animated.View>
+        {/* image indicator of the amount of images and the current index of the image */}
+        <View className='absolute bottom-[0.5] left-0 w-full flex-row justify-center items-center'>
+          <View className='flex-row'>
+            {user.images.map((image, index) => (
+              <View
+                key={image}
+                className={`w-12 h-2 rounded-full ${
+                  index === currentImageIndex
+                    ? 'bg-primaryWhite'
+                    : 'bg-white/40'
+                } mx-1`}
+              />
+            ))}
+          </View>
+        </View>
+      </Animated.View>
+    </>
   );
 }
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -13,6 +13,7 @@ export default function HomeScreen({ navigation }) {
       lastName: 'Doe',
       images: [
         'https://images.unsplash.com/photo-1568000211272-88ae1d40afc1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2570&q=80',
+        'https://images.unsplash.com/photo-1645036811556-d4ce14657484?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
       ],
       age: 25,
       gymId: 'Powerhouse Gym',
@@ -52,7 +53,7 @@ export default function HomeScreen({ navigation }) {
         snapToAlignment='center'
         showsVerticalScrollIndicator={false}
         scrollEventThrottle={16}
-        className='bg-primaryDark'
+        className='bg-primaryDark relative'
         contentInsetAdjustmentBehavior='automatic'
         style={{ paddingHorizontal: 16 }}
         contentContainerStyle={{ justifyContent: 'space-between' }}

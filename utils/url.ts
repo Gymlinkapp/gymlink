52591feb3093fa2d71975b3c940583e49a73279f
Dylan <dylan.reeder5@gmail.com>
2023-01-12 12:27:42 -0500

    change url

--- utils/url.ts
+++ utils/url.ts
@@ -1,4 +1,4 @@
-// export let URL = process.env.NODE_ENV
-//   ? 'http://10.0.1.198:3000'
-//   : 'https://gymlink-service.onrender.com';
-export let URL = 'https://gymlink-service.onrender.com';
+export let URL = process.env.NODE_ENV
+  ? 'http://10.0.1.198:3000'
+  : 'https://gymlink-service.onrender.com';
+// export let URL = 'https://gymlink-service.onrender.com';

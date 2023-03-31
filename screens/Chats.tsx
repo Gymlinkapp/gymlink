682308e5fb181d070227b513ed93613f5468683c
Dylan <dylan.reeder5@gmail.com>
2023-03-31 09:23:36 -0400

    delete chat

--- screens/Chats.tsx
+++ screens/Chats.tsx
@@ -1,11 +1,18 @@
-import { ChatsTeardrop } from 'phosphor-react-native';
-import { FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { ChatsTeardrop, Check, X } from 'phosphor-react-native';
+import { Animated, FlatList, Text, TouchableOpacity, View } from 'react-native';
 import { User } from '../utils/users';
 import EmptyScreen from '../components/EmptyScreen';
 import { useAuth } from '../utils/context';
 import { Image } from 'react-native';
 import { useState } from 'react';
 import { truncate } from '../utils/ui';
+import {
+  RectButton,
+  Swipeable,
+  TouchableOpacity as RNGHOpacity,
+} from 'react-native-gesture-handler';
+import { useMutation, useQueryClient } from 'react-query';
+import api from '../utils/axiosStore';
 
 export type Message = {
   id?: string;
@@ -28,8 +35,57 @@ export interface Chat {
 
 export default function Chats({ navigation, route }: any) {
   const { socket } = route.params;
+  const queryClient = useQueryClient();
+  const [chatId, setChatId] = useState<string | null>(null);
 
   const { user, token } = useAuth();
+
+  const createLink = useMutation(
+    async ({ chatId }: { chatId: string }) => {
+      const { data } = await api.delete(`/chats/${chatId}`);
+      return data;
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries('user');
+      },
+    }
+  );
+
+  const renderRightActions = (progress, dragX) => {
+    const trans = dragX.interpolate({
+      inputRange: [0, 50, 75, 100],
+      outputRange: [0, 0, 0, 1],
+    });
+
+    return (
+      <RectButton>
+        <Animated.View
+          style={[
+            {
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              width: 100,
+              height: '100%',
+              transform: [{ translateX: trans }],
+            },
+          ]}
+        >
+          <RNGHOpacity
+            className='bg-primarydark p-4 rounded-full'
+            onPress={() => {
+              createLink.mutate({
+                chatId: chatId,
+              });
+            }}
+          >
+            <X color='#fff' weight='fill' />
+          </RNGHOpacity>
+        </Animated.View>
+      </RectButton>
+    );
+  };
 
   return (
     <View className='flex-1'>
@@ -49,50 +105,56 @@ export default function Chats({ navigation, route }: any) {
                 (u: User) => u.id !== user.id
               ) as User;
               return (
-                <TouchableOpacity
-                  className='flex-row items-center my-2 bg-secondaryDark p-4 rounded-md'
-                  onPress={() => {
-                    navigation.navigate('Chat', {
-                      socket: socket,
-                      user: user,
-                      roomName: item.name,
-                      toUser: otherUser,
-                      userImage: otherUser.images[0],
-                      uiName: `${otherUser.firstName} ${otherUser.lastName}`,
-                      roomId: item.id,
-                    });
+                <Swipeable
+                  renderRightActions={renderRightActions}
+                  onSwipeableOpen={() => {
+                    setChatId(item.id);
                   }}
                 >
-                  <Image
-                    source={{ uri: otherUser.images[0] }}
-                    className='w-[50px] h-[50px] rounded-full mr-4'
-                  />
-                  <View className='flex-col'>
-                    <Text className='text-white text-xl'>
-                      {otherUser.firstName} {otherUser.lastName}
-                    </Text>
-                    {recentMessage && (
-                      <View className='flex-col items-left'>
-                        <Text className='text-secondaryWhite text-xs mr-2'>
-                          {new Date(recentMessage.createdAt).toLocaleDateString(
-                            'en-US',
-                            {
+                  <TouchableOpacity
+                    className='flex-row items-center my-2 bg-secondaryDark p-4 rounded-md'
+                    onPress={() => {
+                      navigation.navigate('Chat', {
+                        socket: socket,
+                        user: user,
+                        roomName: item.name,
+                        toUser: otherUser,
+                        userImage: otherUser.images[0],
+                        uiName: `${otherUser.firstName} ${otherUser.lastName}`,
+                        roomId: item.id,
+                      });
+                    }}
+                  >
+                    <Image
+                      source={{ uri: otherUser.images[0] }}
+                      className='w-[50px] h-[50px] rounded-full mr-4'
+                    />
+                    <View className='flex-col'>
+                      <Text className='text-white text-xl'>
+                        {otherUser.firstName} {otherUser.lastName}
+                      </Text>
+                      {recentMessage && (
+                        <View className='flex-col items-left'>
+                          <Text className='text-secondaryWhite text-xs mr-2'>
+                            {new Date(
+                              recentMessage.createdAt
+                            ).toLocaleDateString('en-US', {
                               hour: 'numeric',
                               minute: 'numeric',
                               month: 'short',
                               day: 'numeric',
-                            }
-                          )}
-                        </Text>
-                        <Text className='text-white text-md'>
-                          {recentMessage.content.length > 15
-                            ? truncate(recentMessage.content, 30)
-                            : recentMessage.content}
-                        </Text>
-                      </View>
-                    )}
-                  </View>
-                </TouchableOpacity>
+                            })}
+                          </Text>
+                          <Text className='text-white text-md'>
+                            {recentMessage.content.length > 15
+                              ? truncate(recentMessage.content, 30)
+                              : recentMessage.content}
+                          </Text>
+                        </View>
+                      )}
+                    </View>
+                  </TouchableOpacity>
+                </Swipeable>
               );
             }}
           />

4078003959af511fddf4f98065e76d9a9342e5fb
Dylan <dylan.reeder5@gmail.com>
2023-04-17 08:42:23 -0400

    update build number & remove log

--- screens/ProfileInfo.tsx
+++ screens/ProfileInfo.tsx
@@ -40,7 +40,6 @@ export default function ProfileInfo({
   const { user: currUser } = useAuth();
   const [userSplit, setUserSplit] = useState<WeekSplit[]>([]);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  console.log(userSplit);
 
   const { data: gym, isLoading: gymLoading } = useGym(gymId);
 

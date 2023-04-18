1e8b24c2b15aeb88593b6857b555e7668ab511a1
Dylan <dylan.reeder5@gmail.com>
2023-04-18 12:17:38 -0400

    track of total users to not override feed & pagination

--- hooks/useUsers.tsx
+++ hooks/useUsers.tsx
@@ -3,18 +3,31 @@ import { useQuery } from 'react-query';
 import api from '../utils/axiosStore';
 import { User } from '../utils/users';
 
-const fetchUsers = async (token: string) => {
+type Response = {
+  users: User[];
+  totalUsers: number;
+};
+
+const fetchUsers = async (token: string, offset: number = 0, limit: number) => {
   // const { data } = await api.get(`/users/getNearByUsers?token=${token}`);
   const { data } = await api.post(`/users/findNearUsers`, {
     token,
-    offset: 0,
+    offset: offset,
     limit: 9,
   });
-  return data.users;
+  return data;
 };
 
-const useUsers = (token: string) => {
-  return useQuery<User[], Error>(['users', token], () => fetchUsers(token));
+const useUsers = (token: string, offset: number = 0, limit: number) => {
+  return useQuery<Response, Error>(
+    ['users', offset],
+    () => fetchUsers(token, offset, limit),
+    {
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 5,
+      keepPreviousData: true,
+    }
+  );
 };
 
 export { fetchUsers, useUsers };
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,12 @@
-import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import {
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  Animated,
+  Easing,
+} from 'react-native';
 import Layout from '../layouts/layout';
 import React, { useEffect, useRef, useState } from 'react';
 import { useUsers } from '../hooks/useUsers';
@@ -10,8 +18,10 @@ import Filters from '../components/Filters';
 import { FilterType, defaultFilters } from '../utils/types/filter';
 import { useMutation } from 'react-query';
 import api from '../utils/axiosStore';
+import { User } from '../utils/users';
 
 export default function HomeScreen({ navigation, route }) {
+  const INITIAL_COL_OFFSETS = [50, 150, 100];
   const LIMIT = 9;
   const INITIAL_SCROLL_POSITION = 250;
   const { token, user, setUser, filters, setFilters, feed, setFeed } =
@@ -20,9 +30,10 @@ export default function HomeScreen({ navigation, route }) {
   const [columnData, setColumnData] = useState([]);
   const [scrollPosition, setScrollPosition] = useState(INITIAL_SCROLL_POSITION);
   const [offset, setOffset] = useState(0);
-  const { data: users } = useUsers(token);
-  const [isOffsetFeedLoading, setIsOffsetFeedLoading] = useState(false);
-
+  const { data, isLoading, isFetching } = useUsers(token, offset, LIMIT);
+  const [animatedScrolls, setAnimatedScrolls] = useState(
+    INITIAL_COL_OFFSETS.map((offset) => new Animated.Value(offset))
+  );
   const [hasInitialScrolled, setHasInitialScrolled] = useState(false);
 
   function splitArrayIntoColumns(array: any[] = [], numColumns: number) {
@@ -33,18 +44,12 @@ export default function HomeScreen({ navigation, route }) {
     return columnArrays;
   }
 
-  const isLoading = !users;
-
-  function handlescroll(e) {
-    setScrollPosition(e.nativeEvent.contentOffset.y);
-  }
-
   useEffect(() => {
     if (user) {
       setFilters(defaultFilters);
     }
-    if (!isLoading && users) {
-      setFeed(users);
+    if (!isLoading && data && data.users) {
+      setFeed(data.users);
 
       setTimeout(() => {
         flatListRef.current?.scrollToOffset({
@@ -54,7 +59,7 @@ export default function HomeScreen({ navigation, route }) {
         setHasInitialScrolled(true);
       }, 500);
     }
-  }, [isLoading, users, user]);
+  }, [isLoading, data, user]);
 
   useEffect(() => {
     const numColumns = 3;
@@ -74,66 +79,32 @@ export default function HomeScreen({ navigation, route }) {
       };
     });
     setColumnData(columnData);
-  }, [feed]);
 
-  const updateFeed = (newData) => {
-    const updatedFeed = [...feed, ...newData];
-    setFeed(updatedFeed);
-    const updatedColumns = splitArrayIntoColumns(updatedFeed, 3);
-    const updatedColumnData = updatedColumns.map((column, index) => {
-      return {
-        data: column,
-        scrollFactor: columnData[index].scrollFactor,
-      };
-    });
-    setColumnData(updatedColumnData);
+    const newAnimatedScrolls = columnData.map(
+      (column) => new Animated.Value(0)
+    );
+    setAnimatedScrolls(newAnimatedScrolls);
+  }, [feed, data, isFetching]);
+
+  const handleScroll = (event, columnIndex) => {
+    const offsetY = event.nativeEvent.contentOffset.y;
+    animatedScrolls[columnIndex].setValue(offsetY);
   };
 
-  const loadMoreMutation = useMutation(
-    async () => {
-      try {
-        return await api.post(
-          '/users/findNearUsers',
-          {
-            token,
-            offset,
-            limit: LIMIT,
-          },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }
-        );
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    {
-      onSuccess: async (data) => {
-        if (data) {
-          try {
-            const newData = data.data.users;
-            if (newData.length === 0) {
-              console.log('no more');
-              return;
-            }
-
-            if (newData.length < LIMIT) {
-              console.log('less than limit');
-            }
-            updateFeed(newData);
-            setOffset((prev) => prev + LIMIT);
-          } catch (error) {
-            console.log('here', error);
-          }
-        }
-      },
-      onError: (error) => {
-        console.log('uh', error);
-      },
-    }
-  );
+  // const updateFeed = (newData: User[]) => {
+  //   const updatedFeed = newData.filter((incomingUser) => {
+  //     return !feed.find((user) => user.id === incomingUser.id);
+  //   });
+  //   setFeed(updatedFeed);
+  //   const updatedColumns = splitArrayIntoColumns(updatedFeed, 3);
+  //   const updatedColumnData = updatedColumns.map((column, index) => {
+  //     return {
+  //       data: column,
+  //       scrollFactor: columnData[index].scrollFactor,
+  //     };
+  //   });
+  //   setColumnData(updatedColumnData);
+  // };
 
   if (isLoading) {
     return <Loading />;
@@ -156,75 +127,83 @@ export default function HomeScreen({ navigation, route }) {
         keyExtractor={(item) => item.id}
         numColumns={3}
         columnWrapperStyle={{ flex: 1 }}
-        onScroll={handlescroll}
         initialScrollIndex={0}
         initialNumToRender={10}
         removeClippedSubviews={true}
         showsVerticalScrollIndicator={false}
         className='min-h-full'
         onEndReached={() => {
-          if (hasInitialScrolled) {
-            setIsOffsetFeedLoading(true);
-            loadMoreMutation.mutate();
+          if (hasInitialScrolled && offset < data.totalUsers) {
+            setOffset((prev) => prev + LIMIT);
           }
         }}
-        ListFooterComponent={() => loadMoreMutation.isLoading && <Loading />}
+        ListFooterComponent={() => isFetching && <Loading />}
         onEndReachedThreshold={0.1}
         renderItem={({ item: column, index }) => {
+          const scrollY = animatedScrolls[index];
+
           return (
-            <FlatList
+            <Animated.FlatList
               data={column.data} // use the filtered arrays for each column
               keyExtractor={(item) => item.id}
+              onScroll={(event) => handleScroll(event, index)}
+              scrollEventThrottle={128}
               className='flex-1'
-              listKey={`${index + Math.random()}`}
-              style={{
-                transform: [
-                  {
-                    translateY:
-                      -(scrollPosition - INITIAL_SCROLL_POSITION) *
-                      column.scrollFactor,
-                  },
-                ],
-              }}
+              listKey={`column-${index}-${column.id}-${Date.now()}`}
               renderItem={({ item: user, index }) => (
                 <TouchableOpacity
-                  className='h-[250px] m-[0.5px] relative overflow-hidden rounded-3xl justify-end'
+                  key={index}
+                  className='h-[250px] m-[0.5px] relative bg-transparent  justify-end'
                   onPress={() => {
                     navigation.navigate('Profile', {
                       user: user,
                     });
                   }}
                 >
-                  <View className='z-50 p-4'>
-                    <View className='overflow-hidden rounded-full w-8 h-8'>
-                      <BlurView
-                        className='bg-primaryDark/20 w-full h-full rounded-full justify-center items-center'
-                        intensity={25}
-                      >
-                        <Text className=' text-white font-MontserratRegular'>
-                          {user.age}
-                        </Text>
-                      </BlurView>
+                  <Animated.View
+                    className='h-full overflow-hidden justify-end rounded-3xl'
+                    style={{
+                      transform: [
+                        {
+                          translateY: scrollY.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [0, -column.scrollFactor],
+                          }),
+                        },
+                      ],
+                    }}
+                  >
+                    <View className='z-50 p-4'>
+                      <View className='overflow-hidden rounded-full w-8 h-8'>
+                        <BlurView
+                          className='bg-primaryDark/20 w-full h-full rounded-full justify-center items-center'
+                          intensity={25}
+                        >
+                          <Text className=' text-white font-MontserratRegular'>
+                            {user.age}
+                          </Text>
+                        </BlurView>
+                      </View>
+                      <Text className='text-white font-MontserratBold text-xl'>
+                        {user.firstName}
+                      </Text>
                     </View>
-                    <Text className='text-white font-MontserratBold text-xl'>
-                      {user.firstName}
-                    </Text>
-                  </View>
-                  <View className='absolute top-0 left-0 w-full h-full'>
-                    <LinearGradient
-                      pointerEvents='none'
-                      colors={['rgba(0,0,0,1)', 'rgba(0,0,0,0)']}
-                      className='absolute bottom-0 left-0 z-50 w-full h-full'
-                      locations={[0, 0.5]}
-                      // bottom to top
-                      start={[0, 1]}
-                      end={[1, 0]}
-                    />
-                    <Image
-                      source={{ uri: user.images[0] }}
-                      className='w-full h-full object-cover'
-                    />
-                  </View>
+                    <View className='absolute top-0 left-0 w-full h-full'>
+                      <LinearGradient
+                        pointerEvents='none'
+                        colors={['rgba(0,0,0,1)', 'rgba(0,0,0,0)']}
+                        className='absolute bottom-0 left-0 z-50 w-full h-full'
+                        locations={[0, 0.5]}
+                        // bottom to top
+                        start={[0, 1]}
+                        end={[1, 0]}
+                      />
+                      <Image
+                        source={{ uri: user.images[0] }}
+                        className='w-full h-full object-cover'
+                      />
+                    </View>
+                  </Animated.View>
                 </TouchableOpacity>
               )}
             />

2b14a18eea7136053d856accbe9096039f1c0515
Dylan <dylan.reeder5@gmail.com>
2023-04-22 13:25:58 -0400

    added tags

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -49,6 +49,13 @@ export default function HomeScreen({ navigation, route }) {
         start={[0, 0]}
         end={[0, 1]}
       />
+      <LinearGradient
+        pointerEvents='none'
+        colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']}
+        className='absolute bottom-0 z-50 w-full h-80'
+        start={[0, 0]}
+        end={[0, 1]}
+      />
 
       {/* <Filters /> */}
       <FlatList
@@ -63,6 +70,7 @@ export default function HomeScreen({ navigation, route }) {
         renderItem={({ item: user }) => (
           <View className='my-4'>
             <TouchableOpacity
+              activeOpacity={1}
               className='h-60 w-full relative overflow-hidden rounded-[50px]'
               onPress={() => {
                 navigation.navigate('Profile', { user });
@@ -71,6 +79,29 @@ export default function HomeScreen({ navigation, route }) {
               <Image
                 source={{ uri: user.images[0] }}
                 className='object-cover w-full h-full'
+              />
+              <ScrollView
+                horizontal
+                className='absolute bottom-5 left-2 flex-row z-20'
+              >
+                {user.tags.length > 0 &&
+                  user.tags.map((tag, idx) => (
+                    <BlurView
+                      key={idx}
+                      className='bg-primaryDark/20 px-4 py-2 mx-2 rounded-full overflow-hidden border-[0.5px] border-tertiaryDark'
+                      intensity={25}
+                    >
+                      <Text className='font-ProstoOne text-white text-xs '>
+                        {tag}
+                      </Text>
+                    </BlurView>
+                  ))}
+              </ScrollView>
+              <LinearGradient
+                colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.8)']}
+                className='absolute bottom-0 w-full h-60 z-10'
+                start={[0, 0]}
+                end={[0, 1]}
               />
             </TouchableOpacity>
             <TouchableOpacity className='flex-row items-center my-2'>

5b5e5de088a176c04d23d4758f4006126c0b3370
Dylan <dylan.reeder5@gmail.com>
2023-04-22 19:39:04 -0400

    fix crashing with prompts

--- utils/getMostRecentPrompt.ts
+++ utils/getMostRecentPrompt.ts
@@ -1,5 +1,6 @@
 import { User } from './users';
 export default function getMostRecentPrompt(user: User) {
+  if (!user.userPrompts.length) return;
   const lastPrompt = user.userPrompts.reduce((latest, current) => {
     return new Date(current.createdAt) > new Date(latest.createdAt)
       ? current

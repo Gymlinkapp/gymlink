2037f30e947a888547e9cb6c9fa399279b7a6fbf
Dylan <dylan.reeder5@gmail.com>
2023-05-01 19:19:50 -0400

    better location permission flow

--- screens/auth/DiscloseLocationScreen.tsx
+++ screens/auth/DiscloseLocationScreen.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import useLocation from '../../hooks/useLocation';
 import * as Location from 'expo-location';
 import { useAuth } from '../../utils/context';
+import { Barbell, CheckCircle, UsersFour } from 'phosphor-react-native';
 
 export default function DiscloseLocationScreen({
   navigation,
@@ -17,7 +18,7 @@ export default function DiscloseLocationScreen({
     if (permissionStatus === 'granted' && long && lat) {
       navigation.navigate('UserGymLocation');
     }
-    if (permissionStatus === 'denied' || permissionStatus === 'undetermined') {
+    if (permissionStatus === 'denied') {
       promptForPermission();
     }
 
@@ -46,23 +47,42 @@ export default function DiscloseLocationScreen({
   return (
     <AuthLayout
       title='Allow your location'
-      description='We will use your location to make your experience better and provide a tailored experience and allow Gymlink to display nearby gyms and potential like-minded gym goers on your feed.'
+      description='We will use your location to make your experience better and provide a tailored experience.'
     >
+      <View>
+        <View className='flex-row items-center mb-5'>
+          <Barbell size={50} color='#fff' weight='fill' />
+          <Text className='text-secondaryWhite font-MontserratRegular text-lg ml-2'>
+            Find nearby gyms
+          </Text>
+        </View>
+        <View className='flex-row items-center mb-5'>
+          <UsersFour size={50} color='#fff' weight='fill' />
+          <Text className='text-secondaryWhite font-MontserratRegular text-lg ml-2'>
+            Find nearby gym buddies
+          </Text>
+        </View>
+      </View>
       <View className='justify-end h-full'>
-        {permissionStatus === 'granted' && (
-          <Button
-            variant='primary'
-            onPress={() => {
-              if (permissionStatus === 'granted' && long && lat) {
-                navigation.navigate('UserGymLocation');
-              }
-            }}
-          >
-            Allow Location
-          </Button>
-        )}
+        {permissionStatus === 'granted' ||
+          (permissionStatus === 'undetermined' && (
+            <View className='flex-[0.45]'>
+              <Button
+                variant='primary'
+                onPress={() => {
+                  if (permissionStatus === 'granted' && long && lat) {
+                    navigation.navigate('UserGymLocation');
+                  } else {
+                    promptForPermission();
+                  }
+                }}
+              >
+                Allow Location
+              </Button>
+            </View>
+          ))}
         {permissionStatus === 'denied' && (
-          <View>
+          <View className='flex-[0.65]'>
             <Text className='text-secondaryWhite font-MontserratRegular text-xs mb-10'>
               You have denied location permission. Gymlink may have unexpected
               behaviour and bugs. If you change your mind, you can allow
@@ -77,11 +97,15 @@ export default function DiscloseLocationScreen({
             >
               Retry
             </Button>
+            <Button
+              variant='ghost'
+              className='mb-10 my-5'
+              onPress={openSettings}
+            >
+              Go to Settings
+            </Button>
           </View>
         )}
-        <Button variant='ghost' className='mb-10 my-5' onPress={openSettings}>
-          Go to Settings
-        </Button>
       </View>
     </AuthLayout>
   );

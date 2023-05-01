0d80d57710db10769f9ea2201fc5a3814188d67a
Dylan <dylan.reeder5@gmail.com>
2023-05-01 19:01:45 -0400

    update permission strings and location disclosure

--- screens/auth/DiscloseLocationScreen.tsx
+++ screens/auth/DiscloseLocationScreen.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { Text, View, Linking, Platform } from 'react-native';
 import AuthLayout from '../../layouts/AuthLayout';
 import Button from '../../components/button';
 import { useEffect } from 'react';
@@ -31,23 +31,55 @@ export default function DiscloseLocationScreen({
       })();
     }
   }, [permissionStatus, long, lat]);
+
+  const openSettings = async () => {
+    const settingsURL =
+      Platform.OS === 'ios' ? 'app-settings:' : 'app-settings:';
+    const canOpen = await Linking.canOpenURL(settingsURL);
+
+    if (canOpen) {
+      Linking.openURL(settingsURL);
+    } else {
+      console.error('Failed to open app settings');
+    }
+  };
   return (
     <AuthLayout
       title='Allow your location'
-      description='We will use your location to make your experience better and provide a closer and richer connection with nearby users.'
+      description='We will use your location to make your experience better and provide a tailored experience and allow Gymlink to display nearby gyms and potential like-minded gym goers on your feed.'
     >
       <View className='justify-end h-full'>
-        <Button
-          variant='primary'
-          onPress={() => {
-            if (permissionStatus === 'granted' && long && lat) {
-              navigation.navigate('UserGymLocation');
-            }
-          }}
-        >
-          Allow Location
-        </Button>
-        <Button variant='ghost' className='mb-10 my-5'>
+        {permissionStatus === 'granted' && (
+          <Button
+            variant='primary'
+            onPress={() => {
+              if (permissionStatus === 'granted' && long && lat) {
+                navigation.navigate('UserGymLocation');
+              }
+            }}
+          >
+            Allow Location
+          </Button>
+        )}
+        {permissionStatus === 'denied' && (
+          <View>
+            <Text className='text-secondaryWhite font-MontserratRegular text-xs mb-10'>
+              You have denied location permission. Gymlink may have unexpected
+              behaviour and bugs. If you change your mind, you can allow
+              location permission in your settings.
+            </Text>
+            <Button
+              variant='ghost'
+              className='mb-10 my-5'
+              onPress={() => {
+                promptForPermission();
+              }}
+            >
+              Retry
+            </Button>
+          </View>
+        )}
+        <Button variant='ghost' className='mb-10 my-5' onPress={openSettings}>
           Go to Settings
         </Button>
       </View>
